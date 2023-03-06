e36571f4d2b777136ada911a019dacfdee92acb5
Viktoriia38 <bva922@ukr.net>
2023-03-06 22:57:46 +0200

    Routes in App + Private Route + Public Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
+import { Routes, Route, Navigate } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import { Loader } from './Loader/Loader';
+// import { useDispatch } from 'react-redux';
+// import { getCurrentUserRequest } from 'redux/authOperations';
+import { PublicRoute } from './PublicRoute/PublicRoute';
+import { PrivateRoute } from './PrivateRoute/PrivateRoute';
+
+const LazyLayout = lazy(() => import('./Layout/Layout'));
+const LazyHomepage = lazy(() => import('../pages/HomePage/HomePage'));
+const LazyLoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const LazyRegisterPage = lazy(() =>
+  import('../pages/RegistrationPage/RegistrationPage')
+);
+const LazyDiaryPage = lazy(() => import('../pages/DiaryPage/DiaryPage'));
+
+function App() {
+  // const dispatch = useDispatch();
+
+  //   useEffect(() => {
+  //     dispatch(getCurrentUserRequest());
+  //   }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<LazyLayout />}>
+          <Route index element={<LazyHomepage />} />
+          <Route path="/" element={<PublicRoute />}>
+            <Route path="/register" element={<LazyRegisterPage />} />
+            <Route path="/login" element={<LazyLoginPage />} />
+          </Route>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/diary" element={<LazyDiaryPage />} />
+            <Route path="/calculator" element={<LazyCalculatorPage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
-};
+}
+
+export default App;
