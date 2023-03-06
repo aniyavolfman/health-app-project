3d2f5c12752f74a26301ca4537b5bd9904969d72
Iuliia Goriun <goriunyuliya@gmail.com>
2023-03-07 00:04:11 +0300

    Layout, HomePage, Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+//import YourSvg from "/path/to/image.svg";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <a href="../../pages/HomePage">
+          {/* <img src={YourSvg} alt="logo" /> */}
+          SlimMom
+        </a>
+      </header>
+    </>
+  );
+}
