9c5131e726d87922f6096866800c7f62c991cc81
Natalia Levenets <levenets.lot@gmail.com>
2023-03-07 15:31:47 +0200

    Loader+backdrop for loading

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import { Oval } from 'react-loader-spinner';
 
+export function Loader () {
+  return (
+    <>
+     <div style={{position: 'fixed', top: 0, width: '100%', height: '100%', background: 'rgba(0,0,0, 0.5)', }}>
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={8}
+        strokeWidthSecondary={8}
+      />
+    </div>
+     </>
+  );
+};
