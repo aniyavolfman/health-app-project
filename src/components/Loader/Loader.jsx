a9df8bb1d2e29f8057354f1ca20e1a76f8c20871
Natalia Levenets <levenets.lot@gmail.com>
2023-03-07 14:14:11 +0200

    Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import { Oval } from 'react-loader-spinner';
 
+export function Loader () {
+  return (
+    <>
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={8}
+        strokeWidthSecondary={8}
+      />
+    </>
+  );
+};
