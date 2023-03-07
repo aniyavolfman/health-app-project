86660512eae37b076e740c9a36dab6fdded3c27d
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-03-07 19:51:36 +0200

    added

--- src/components/ModalRec/ModalRec.jsx
+++ src/components/ModalRec/ModalRec.jsx
@@ -2,34 +2,59 @@ import css from './ModalRec.module.scss';
 // import { MdClose } from 'react-icons/md';
 import { MdKeyboardReturn } from 'react-icons/md';
 
-import React from 'react';
+// import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+// import { useDispatch } from 'react-redux';
+
+const modalRoot = document.getElementById('modal-root');
 
 export function ModalRec() {
+  // const dispatch = useDispatch();
+
+  //   useEffect(() => {
+  //     const onEscapeClick = (event) => {
+  // if (event.code === 'Escape') dispatch(closeModal())
+  //     }
+
+  //     window.addEventListener('keydown', onEscapeClick);
+
+  //     return () => {
+  //       window.removeEventListener('keydown', onEscapeClick)
+  //     }
+  //   }, [dispatch]);
+
+  // const onBackdropClick = event => {
+  //   if (event.target === event.currentTarget) dispatch(closeModal());
+  // };
+
+  // onClick = { onBackdropClick };
   return (
-    <div className={css.recBackdrop}>
-      <div className={css.recModal}>
-        <button type="button" className={css.closeBtn} width="20">
-          <MdKeyboardReturn width="12" height="7" />
-        </button>
-        <h2 className={css.recTitle}>
-          Your recommended daily calorie intake is
-        </h2>
-        <p className={css.recElText}>
-          <span className={css.recEl}>2800</span>ккал
-        </p>
-        <div class={css.recLine}></div>
-        <div className={css.recContent}>
+    createPortal(
+      <div className={css.recBackdrop}>
+        <div className={css.recModal}>
+          <button type="button" className={css.closeBtn} width="20">
+            <MdKeyboardReturn width="12" height="7" />
+          </button>
+          <h2 className={css.recTitle}>
+            Your recommended daily calorie intake is
+          </h2>
+          <p className={css.recElText}>
+            <span className={css.recEl}>2800</span>ккал
+          </p>
+
+          <div className={css.recLine}></div>
           <p className={css.recText}>Foods you should not eat</p>
           <ol className={css.recList}>
             <li className={css.recItem}>Flour products</li>
             <li>Milk</li>
           </ol>
-        </div>
 
-        <button type="submit" class={css.btnSubmit}>
-          Start losing weight
-        </button>
+          <button type="submit" class={css.btnSubmit}>
+            Start losing weight
+          </button>
+        </div>
       </div>
-    </div>
+    ),
+    modalRoot
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import CalculatorСalorieForm from 'components/CalculatorСalorieForm/CalculatorСalorieForm';
+import { ModalRec } from 'components/ModalRec/ModalRec';
 import React from 'react';
 
 export default function HomePage() {
   return (
     <div>
       <CalculatorСalorieForm />
+      <ModalRec />
     </div>
   );
 }
