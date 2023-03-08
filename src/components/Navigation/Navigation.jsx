0184675a4524ce882539916e4c7ba5af93b1947f
Anna Volfman <aniya_07@ukr.net>
2023-03-08 15:58:10 +0200

    reUpdate

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { selectAccessToken } from 'redux/auth/authSelectors';
+import css from '../Navigation/Navigation.module.css';
+
+export default function Navigation() {
+  const token = useSelector(selectAccessToken);
+
+  return (
+    <nav className={css.navigation}>
+      {token ? (
+        <div className={css.navigationMenu}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? css.active : css.navigationLink
+            }
+            to="/diary"
+          >
+            DIARY
+          </NavLink>
+          <NavLink
+            to="/calculator"
+            className={({ isActive }) =>
+              isActive ? css.active : css.navigationLink
+            }
+          >
+            CALCULATOR
+          </NavLink>
+        </div>
+      ) : (
+        <div className={css.navigationMenu}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? css.active : css.navigationLink
+            }
+            to="/login"
+          >
+            LOG IN
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? css.active : css.navigationLink
+            }
+            to="/register"
+          >
+            REGISTRATION
+          </NavLink>
+        </div>
+      )}
+    </nav>
+  );
+}
