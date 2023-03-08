7f7d35372d36b3ddb7474583cf9a0d2d9cccba86
Natalia Levenets <levenets.lot@gmail.com>
2023-03-08 14:09:36 +0200

    commit logout - deleted
    1. Удалила тестовую кнопку логаут
    2. удалила лишний импорт из api

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,17 +3,10 @@ import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { selectAccessToken } from 'redux/auth/authSelectors';
 import css from '../Navigation/Navigation.module.css';
-import { useDispatch } from 'react-redux';
-import {  logOutRequest  } from 'redux/auth/authOperations';
 
 
 export default function Navigation() {
   const token = useSelector(selectAccessToken);
-  const dispatch = useDispatch();
-  const handleLogout = () => {
-    dispatch(logOutRequest());
-  };
-
   return (
     <nav className={css.navigation}>
       {token ? (
@@ -34,9 +27,6 @@ export default function Navigation() {
           >
             CALCULATOR
           </NavLink>
-          <button  type="button" onClick={handleLogout}>
-          LogOut
-        </button> 
         </div>
       ) : (
         <div className={css.navigationMenu}>
