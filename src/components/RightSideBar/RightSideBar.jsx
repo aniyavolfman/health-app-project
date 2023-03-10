55d58cbd4bdb74d968708ad0ca4890a7b0ed521c
Maryna <yasinskamarina@gmail.com>
2023-03-10 21:05:46 +0200

    add sideBar+styleForm

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -6,52 +6,63 @@ import { nanoid } from 'nanoid';
 export default function RightSideBar() {
   const date = useSelector(state => state.products.currentDate);
   const notAllowedProducts = useSelector(
-    state => state.auth.userData.notAllowedProducts);
+    state => state.auth.userData.notAllowedProducts
+  );
   const summaries = useSelector(state => state.dailyRate.summaries);
-
 
   const shortProducts = notAllowedProducts?.slice(0, 9);
 
   return (
-    <div className={css.userInfo}>
-      <h2 className={css.userInfoTitle}>Summary for {date}</h2>
-      <ul className={css.userInfoList}>
-        <li className={css.userInfoItem}>
-          Left
-          <span className={css.userInfoData}>
+    <div className={css.SidebarSection}>
+      <div className={css.SidebarWrap}>
+        <div className={css.SidebarRecomend}>
+          <h2 className={css.userInfoTitle}>Summary for {date}</h2>
+          <ul className={css.SidebarList}>
+            <li className={css.SidebarItem}>
+              <p className={css.SidebarText}>Залишилось</p>
+              <p className={css.SidebarText}>
+                {summaries?.kcalLeft.toFixed(2)} ккал
+              </p>
+              {/* <span className={css.userInfoData}>
             {summaries?.kcalLeft.toFixed(2)} ккал
-          </span>
-        </li>
-        <li className={css.userInfoItem}>
-          Consumed
-          <span className={css.userInfoData}>
-            {summaries?.kcalConsumed.toFixed(2)} ккал
-          </span>
-        </li>
-        <li className={css.userInfoItem}>
-          Daily rate
-          <span className={css.userInfoData}>
-            {summaries?.dailyRate.toFixed(2)} ккал
-          </span>
-        </li>
-        <li className={css.userInfoItem}>
-          % від норми
-          <span className={css.userInfoData}>
-            {summaries?.percentsOfDailyRate.toFixed(2)} %
-          </span>
-        </li>
-      </ul>
-      <h2 className={css.userInfoTitle}>Не рекомендовані продукти</h2>
-      {!shortProducts && <p className={css.userInfoDescription}>
-        Your diet will be displayed here
-      </p>}
-      <ul>
-        {shortProducts?.map(el => (
-          <li key={nanoid()}>
-            <span>{el}</span>
-          </li>
-        ))}
-      </ul>
+          </span> */}
+            </li>
+            <li className={css.SidebarItem}>
+              <p className={css.SidebarText}>Спожито</p>
+              <p className={css.SidebarText}>
+                {summaries?.kcalConsumed.toFixed(2)} ккал
+              </p>
+            </li>
+            <li className={css.SidebarItem}>
+              <p className={css.SidebarText}>Денна норма</p>
+              <p className={css.SidebarText}>
+                {summaries?.dailyRate.toFixed(2)} ккал
+              </p>
+            </li>
+            <li className={css.SidebarItem}>
+              <p className={css.SidebarText}>% від норми</p>
+              <p className={css.SidebarText}>
+                {summaries?.percentsOfDailyRate.toFixed(2)} %
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className={css.SidebarNotRecomend}>
+          <h2 className={css.userInfoTitle}>Не рекомендовані продукти</h2>
+          {!shortProducts && (
+            <p className={css.userInfoDescription}>
+              Your diet will be displayed here
+            </p>
+          )}
+          <ul>
+            {shortProducts?.map(el => (
+              <li key={nanoid()}>
+                <span>{el}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
